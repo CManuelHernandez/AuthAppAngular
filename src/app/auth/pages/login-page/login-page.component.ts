39c0ac5e9d69b5c0d5541e9ae76cc08a45dbadc5
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-14 13:56:39 +0200

    Function Guards

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,6 +13,7 @@ import Swal from 'sweetalert2';
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   public myForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -22,7 +24,7 @@ export class LoginPageComponent {
     const { email, password } = this.myForm.value;
 
     this.authService.login(email, password).subscribe({
-      next: () => console.log('Todo Bien!'),
+      next: () => this.router.navigateByUrl('/dashboard'),
       error: (message) => {
         Swal.fire('Error', message, 'error');
       },
